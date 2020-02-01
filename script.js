901e99e0df44196dab68fc0c034487b19cedfcb3
Sergio Ramirez <s1nicolasrg@gmail.com>
2020-02-01 02:22:46 -0500

    Se implementa preventDefault para evitar que se agregue # en el link

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let boton= document.getElementById("icono");
 let enlaces= document.getElementById("enlaces");
 let contador = 0;
-boton.addEventListener("click", function(){
+boton.addEventListener("click", function(e){
+    e.preventDefault();
     if (contador==0){
         enlaces.className = ("enlaces dos")
         contador=1;
